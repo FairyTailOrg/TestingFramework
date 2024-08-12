2f2dc82e5b2ece7ef5510a961e9ca42e8368b164
Victor Mendoza <victor_mf20@hotmail.com>
2024-08-12 10:31:35 -0600

    Added new changes for gitignore, configuration in pytest ini and condafile

--- tests/web/test_login.py
+++ tests/web/test_login.py
@@ -1,17 +1,6 @@
 from tests.web.basetestcase import BaseTestCase
 
-class TestLogin(BaseTestCase):
 
+class TestLogin(BaseTestCase):
     def test_valid_login(self):
         self.navigate_to("https://example.com/login")
-        self.fill_field("#username", "testuser")
-        self.fill_field("#password", "testpass")
-        self.click_button("#login")
-        self.assert_text_in_element("body", "Welcome")
-
-    def test_invalid_login(self):
-        self.navigate_to("https://example.com/login")
-        self.fill_field("#username", "wronguser")
-        self.fill_field("#password", "wrongpass")
-        self.click_button("#login")
-        self.assert_text_in_element("body", "Invalid credentials")
