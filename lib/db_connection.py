95fb35f7041c51426ee7e755be0a054717b29bde
Victor Mendoza <victor_mf20@hotmail.com>
2024-08-20 13:00:56 -0600

    Added support to connect to a dB

---
+++ lib/db_connection.py
@@ -0,0 +1,25 @@
+"""This file will content how to connect to dB."""
+import pymssql
+
+
+class DbConnection:
+    """This class will establishes connection with a dB."""
+    def __init__(self, user, password, dbname, host, port) -> None:
+        self.user = user
+        self.password = password
+        self.dbname = dbname
+        self.host = host
+        self.port = port
+    
+    def connect_to_db(self):
+            """Connect to database. 2 instances are needed because mngr and db.
+            """
+            # Connection for db 
+            self.conn  = pymssql.connect(server=self.host, user=self.user, password=self.password, database=self.dbname, port=1433)
+            self.cursor = self.conn.cursor(as_dict=True)
+        
+    def close_db(self):
+        """Close db connections
+        """
+        self.cursor.close()
+        self.conn.close()
