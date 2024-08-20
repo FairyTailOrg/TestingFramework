e6ffe3f54113973cb1ff251f80f170fa1875a8b9
Victor Mendoza <victor_mf20@hotmail.com>
2024-08-20 13:06:10 -0600

    Fixed pycodestyle issues

--- lib/db_connection.py
+++ lib/db_connection.py
@@ -4,7 +4,17 @@ import pymssql
 
 class DbConnection:
     """This class will establishes connection with a dB."""
+
     def __init__(self, user, password, dbname, host, port) -> None:
+        """Process parameters.
+        
+        Args:
+            user (string): user to access to dB.
+            password (string): password to access to dB.
+            dbname (string): db to be accesed.
+            host (string): host to access to dB.
+            port (int): port to conect.
+        """
         self.user = user
         self.password = password
         self.dbname = dbname
@@ -12,14 +22,12 @@ class DbConnection:
         self.port = port
     
     def connect_to_db(self):
-            """Connect to database. 2 instances are needed because mngr and db.
-            """
+            """Connect to database. 2 instances are needed because mngr and db."""
             # Connection for db 
             self.conn  = pymssql.connect(server=self.host, user=self.user, password=self.password, database=self.dbname, port=1433)
             self.cursor = self.conn.cursor(as_dict=True)
         
     def close_db(self):
-        """Close db connections
-        """
+        """Close db connections."""
         self.cursor.close()
         self.conn.close()
